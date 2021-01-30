d6885b7beb9d905a8582654e96ead648f31bcc15
Patrik Nilsson <patriknilson@hotmail.com>
2021-01-30 02:38:34 +0100

    Non-VR Interface
    * Non-VR player prefab added which allows the player to move with WASD and pick up items with Left-Click.
    * Default Grabbable Object prefab added which should be the base for all objects in the game. The base mesh is set to a book as default.

---
+++ Assets/Script/non-VR controllers/nonVRTouchStick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nonVRTouchStick : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Trying to grab.");
+            if (other.GetComponent<nonVRGrabableObject>() != null)
+            {
+                Debug.Log("Grabbing!!!");
+                other.GetComponent<nonVRGrabableObject>().Grab();
+            }
+        }
+    }
+}
