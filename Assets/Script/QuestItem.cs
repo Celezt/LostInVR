55d9c02b50cb0db48d67aa41c97512b694c05324
Celezt <celezt@github.com>
2021-01-30 02:01:06 +0100

    added quest item
    - item type
    - score
    - time

---
+++ Assets/Script/QuestItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestItem : MonoBehaviour
+{
+    public enum Item
+    {
+        Test1,
+        Test2,
+        Test3,
+    }
+
+    public Item ItemType;
+    public int Score;
+    public float Time;
+}
