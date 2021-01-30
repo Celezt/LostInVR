d56b8068a428cc4479259328060538fb3c24e201
Celezt <celezt@github.com>
2021-01-30 11:29:19 +0100

    Added Bezier Path Creator
    - follow path component

---
+++ Assets/Script/ClientAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ClientAI : MonoBehaviour
+{
+    public GameObject Player;
+    public int DistanceThreshold = 3;
+
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (Vector3.Distance(Player.transform.position, transform.position) < DistanceThreshold)
+            print("hej");
+    }
+}
