3eb682e1db1739fc3873fb81877933174f5d4be2
Patrik Nilsson <patriknilson@hotmail.com>
2021-01-30 16:32:59 +0100

    Floor counts items
    The floor now count the amount of items on it, and if a lot has fallen onto it recently.

---
+++ Assets/FloorSensor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorSensor : MonoBehaviour
+{
+    public int ItemsOnFloor = 0;
+    public int RecentItemCount = 0;
+    public float Timer;
+    public float TimerTime = 5;
+
+    void Update()
+    {
+        if (Timer > 0f)
+        {
+            Timer -= Time.deltaTime; if (Timer <= 0f)
+            {
+                RecentItemCount = 0;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Object")
+        {
+            ItemsOnFloor += 1;
+            RecentItemCount += 1;
+            if (Timer <= 0f)
+            {
+                Timer = TimerTime;
+            }
+        }
+    }
+}
