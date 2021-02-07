580c4099993d96c2e1838dd94b4e4029c37ffcdc
Thegemsearcher <jacklewitan@hotmail.com>
2021-02-07 12:40:39 +0100

    sounds for HandColliderCubes
    HandColliderCube

---
+++ Assets/Script/GrabSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabSound : MonoBehaviour
+{
+
+    public AudioClip grab;
+    AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GrabbableObject"))
+        {
+            audioSource.PlayOneShot(grab, 0.5f);
+        }
+    }
+}
