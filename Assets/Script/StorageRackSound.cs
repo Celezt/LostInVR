c01cf0c50acb9419af3b14c23ac27ea7443c7a4a
mciarlante <matthew.ciarlante@gmail.com>
2021-02-07 17:17:25 -0600

    Added VRGrabSound and StorageRackSound

---
+++ Assets/Script/StorageRackSound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorageRackSound : MonoBehaviour
+{
+    //private float soundWaitTimer = 0;   
+    private float soundwaittime = 0.2f;
+    private bool soundplayed = false;
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (soundplayed)
+        //{
+        //    soundWaitTimer += Time.deltaTime;
+        //    if (soundWaitTimer > soundwaittime)
+        //    {
+        //        soundplayed = false;
+        //    }
+        //}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (!soundplayed)
+        {
+            AkSoundEngine.PostEvent("Footsteps", gameObject);
+            soundplayed = true;
+            coroutine = DelaySoundReplay();
+            StartCoroutine(coroutine);
+        }
+    }
+
+    IEnumerator DelaySoundReplay()
+    {
+        yield return new WaitForSeconds(soundwaittime);
+        soundplayed = false;
+    }
+}
