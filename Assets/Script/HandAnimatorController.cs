b8f0b12480d6e73c999b393e92440c0b11837de2
mciarlante <matthew.ciarlante@gmail.com>
2021-02-07 19:16:10 -0600

    Fixed grab sound error - hands now open and close in VR

---
+++ Assets/Script/HandAnimatorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimatorController : MonoBehaviour
+{
+    public Animator HandAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseHand()
+    {
+        HandAnimator.SetInteger("Pose", 1);
+    }
+    public void OpenHand()
+    {
+        HandAnimator.SetInteger("Pose", 0);
+    }
+}
